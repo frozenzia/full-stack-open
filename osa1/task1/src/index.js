3f2d3f50a0e58ab1f0f261a65ffcd8f5922d8d65
Paul Vinson Brown <paul.brown@mukavait.fi>
2018-10-30 21:03:30 +0200

    Define parts as objects

--- osa1/task1/src/index.js
+++ osa1/task1/src/index.js
@@ -7,22 +7,24 @@ import Yhteensa from './Yhteensa';
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const osa1 = {
+    nimi: 'Reactin perusteet',
+    tehtavia: 10
+  }
+  const osa2 = {
+    nimi: 'Tiedonvälitys propseilla',
+    tehtavia: 7
+  }
+  const osa3 = {
+    nimi: 'Komponenttien tila',
+    tehtavia: 14
+  }
 
   return (
     <div>
         <Otsikko course={kurssi} />
-        <Sisalto
-            name1={osa1} taskAmt1={tehtavia1}
-            name2={osa2} taskAmt2={tehtavia2}
-            name3={osa3} taskAmt3={tehtavia3}
-        />
-        <Yhteensa val1={tehtavia1} val2={tehtavia2} val3={tehtavia3} />
+        <Sisalto part1={osa1} part2={osa2} part3={osa3} />
+        <Yhteensa val1={osa1.tehtavia} val2={osa2.tehtavia} val3={osa3.tehtavia} />
     </div>
   )
 }
