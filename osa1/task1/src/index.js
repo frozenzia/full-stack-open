04175b7750c64bb02ae2f6569b5ca8055e9017cf
Paul Vinson Brown <paul.brown@mukavait.fi>
2018-11-22 15:27:42 +0200

    Turn function creation into function call (1.10)
    I have been used to placing ```() => {blabla}``` whenever passing an 
    eventHandler. Instead, now this "creation of function" is replaced with 
    a simple function call, and the creation of the function is moved to the 
    function which is being CALLED. (Not confusing at all.)

--- osa1/task1/src/index.js
+++ osa1/task1/src/index.js
@@ -13,7 +13,7 @@ class App extends Component {
         bad: 0,
     };
 
-    addOne = (option) => {
+    addOne = (option) => () => {
         this.setState((prevState) => {
             return { [option]: prevState[option] += 1 };
         });
@@ -23,15 +23,18 @@ class App extends Component {
         return (
             <div>
                 <Otsikko text={strings.headers.giveFeedback} />
-                <Button text={strings.labels.good} handleClick={() => {
-                    this.addOne('good');
-                }} />
-                <Button text={strings.labels.neutral} handleClick={() => {
-                    this.addOne('neutral');
-                }} />
-                <Button text={strings.labels.bad} handleClick={() => {
-                    this.addOne('bad');
-                }} />
+                <Button
+                    text={strings.labels.good}
+                    handleClick={this.addOne('good')}
+                />
+                <Button
+                    text={strings.labels.neutral}
+                    handleClick={this.addOne('neutral')}
+                />
+                <Button
+                    text={strings.labels.bad}
+                    handleClick={this.addOne('bad')}
+                />
                 <Statistics feedback={this.state} />
             </div>
         )
