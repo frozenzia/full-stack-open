a47ef02cfa2396a4609d7f8757f5ed15cc75fe8d
Paul Vinson Brown <paul.brown@mukavait.fi>
2020-02-12 12:23:38 +0200

    Get working with config.js

--- osa4/src/index.js
+++ osa4/src/index.js
@@ -1,12 +1,13 @@
 const http = require('http')
 const express = require('express')
-require('dotenv').config();
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const config = require('../utils/config');
+
+const MONGODB_URI = config.MONGODB_URI;
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -40,7 +41,7 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = process.env.PORT;
+const PORT = config.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
