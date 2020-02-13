c67d47b94cf9291f6415bfaac7ffdd97befd5e40
Paul Vinson Brown <paul.brown@mukavait.fi>
2020-02-13 14:13:53 +0200

    Do coding for 4.5

--- osa4/tests/list_helper.test.js
+++ osa4/tests/list_helper.test.js
@@ -1,33 +1,34 @@
 const listHelper = require('../utils/list_helper');
 
+const sampleBlogs = [
+  {
+    title: "oneLike",
+    author: "haa",
+    url: "https://uta.fi",
+    likes: 1,
+    id: "5e43c32039a51635bc8842f1"
+  },
+  {
+    title: "twoLikes",
+    author: "hoo",
+    url: "https://tuni.fi",
+    likes: 2,
+    id: "5e43c8a339a51635bc8842f2"
+  },
+  {
+    title: "noLikesKeyAtAll",
+    author: "def",
+    url: "https://npr.org",
+    id: "5e43d985a0bce73e0b10da42"
+  },
+];
+
 it('dummy returns one', () => {
   const blogs = [];
   expect(listHelper.dummy(blogs)).toBe(1);
 });
 
 describe('totalLikes', () => {
-  const sampleBlogs = [
-    {
-      title: "oneLike",
-      author: "haa",
-      url: "https://uta.fi",
-      likes: 1,
-      id: "5e43c32039a51635bc8842f1"
-    },
-    {
-      title: "twoLikes",
-      author: "hoo",
-      url: "https://tuni.fi",
-      likes: 2,
-      id: "5e43c8a339a51635bc8842f2"
-    },
-    {
-      title: "noLikesKeyAtAll",
-      author: "def",
-      url: "https://npr.org",
-      id: "5e43d985a0bce73e0b10da42"
-    },
-  ];
   it('of empty array is 0', () => {
     expect(listHelper.totalLikes([])).toBe(0);
   });
@@ -37,5 +38,18 @@ describe('totalLikes', () => {
   });
   it('of array with multiple items calculates correctly, EVEN if some item has no `likes` key.', () => {
     expect(listHelper.totalLikes(sampleBlogs)).toBe(3);
+  });
+});
+
+describe('favoriteBlog', () => {
+  it('of empty array is {}', () => {
+    expect(listHelper.favoriteBlog([])).toEqual({});
+  });
+  it('of array with single item is that item, EVEN if item has no likes', () => {
+    const singleItemArray = [sampleBlogs[2]]; // no likes at all
+    expect(listHelper.favoriteBlog(singleItemArray)).toEqual(sampleBlogs[2]);
+  });
+  it('of array with multiple items calculates correctly, EVEN if some item has no `likes` key.', () => {
+    expect(listHelper.favoriteBlog(sampleBlogs)).toEqual(sampleBlogs[1]);
   });
 });
--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,9 +1,22 @@
 const dummy = () => 1; // (blogs) later...
 
-const totalLikes = (blogs) => blogs.reduce((accum, blog) => accum + (blog.likes || 0), 0);
+const totalLikes = blogs => blogs.reduce((accum, blog) => accum + (blog.likes || 0), 0);
 // NOTE: since 'likes' key is not mandatory, add 0 if not present
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  if (blogs.length === 1) {
+    return blogs[0];
+  }
+  const mostLikes = blogs.reduce((maxSoFar, blog) => Math.max(maxSoFar, (blog.likes || 0)), 0)
+  const mostLikesIndex = blogs.findIndex(blog => ((blog.likes || 0) === mostLikes)); // finds 1st blog with this many likes
+  return blogs[mostLikesIndex];
+}
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
