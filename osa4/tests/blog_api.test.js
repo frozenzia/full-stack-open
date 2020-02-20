44804409819252660e09d9de4b9c9bc8556c399b
Paul Vinson Brown <paul.brown@mukavait.fi>
2020-02-20 08:43:34 +0200

    Do coding for tasks 4.8 - 4.10

---
+++ osa4/tests/blog_api.test.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../src/app')
+const Blog = require('../models/blog');
+// const helper = require('./test_helper');
+
+const api = supertest(app)
+
+// const initialNotes = helper.initialNotes;
+const initialBlogs = [
+  {
+    title: 'Start here',
+    author: 'Not required',
+    url: 'gotta be a 10-char url',
+    likes: 4,
+  },
+  {
+    title: 'Middle o\' nowhere',
+    author: 'Laust',
+    url: 'y.a.c.k.holymancollege',
+    likes: 2,
+  },
+  {
+    title: 'End here',
+    url: 'why do you think this is an url?',
+    likes: 1,
+  },
+];
+
+
+beforeEach(async () => {
+  // empty db
+  await Blog.deleteMany({});
+
+  // populate db
+  let blogObject = null;
+  for (let blog of initialBlogs) {
+    blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+
+});
+
+it('get /api/blogs gives a list of all the blogs on record', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+  expect(response.body.length === initialBlogs.length)
+});
+
+it('blogs returned have a field "id" (not "_id")', async () => {
+  const response = await api
+    .get('/api/blogs')
+  expect(response.body[0].id).toBeDefined();
+});
+
+it('can add a blog to the database', async () => {
+  const newBlog = new Blog({
+    title: 'Lonely writer',
+    author: 'SpedeSpede',
+    url: 'www.fi.fi.fi.fi.fi.fi.fi',
+    likes: 8,
+  });
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+  const response = await api
+    .get('/api/blogs')
+  expect(response.body.length).toEqual(initialBlogs.length + 1);
+  const titles = response.body.map(b => b.title);
+  expect(titles).toContain('Lonely writer');
+});
+
+
+afterAll(() => {
+  mongoose.connection.close()
+})
