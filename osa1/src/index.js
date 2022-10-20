0b9fb8efccff513ed7934a08262b6063fa0073d4
Paul Vinson Brown <paul.brown@mukavait.fi>
2022-10-20 09:29:53 +0300

    Change last line of index.js to new format
    (accepted by React 18)

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -50,4 +50,4 @@ const anecdotes = [
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'))
+ReactDOM.createRoot(document.getElementById('root')).render(<App anecdotes={anecdotes} />)
