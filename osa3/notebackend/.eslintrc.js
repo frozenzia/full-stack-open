b1b0a62056c5b82c22496e0007a7f1b93c11a9c9
Paul Vinson Brown <paul.brown@mukavait.fi>
2022-11-10 23:28:23 +0200

    Adjust ESLint indent rule and fix indent errors

--- osa3/notebackend/.eslintrc.js
+++ osa3/notebackend/.eslintrc.js
@@ -14,17 +14,17 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
-      "indent": [
-        "error",
-        2
-      ],
-      "eqeqeq": "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": [
-          "error", "always"
-      ],
-      "arrow-spacing": [
-          "error", { "before": true, "after": true }
-      ]
+        "indent": [
+            "error",
+            4
+        ],
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ]
     }
 };
