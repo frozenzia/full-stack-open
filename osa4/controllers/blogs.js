55a913edcf34450825ac969866968773734bab60
Paul Vinson Brown <paul.brown@mukavait.fi>
2022-11-11 22:30:04 +0200

    Add `express-async-errors`

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -4,68 +4,52 @@ const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
 const User = require('../models/user');
 
-blogsRouter.get('/', async (request, response, next) => {
-  try {
-    const blogs = await Blog
-      .find({})
-      .populate('user', { username: 1, name: 1, id: 1 });
-    response.json(blogs.map(blog => blog.toJSON()))
-  } catch(e) {
-    next(e);
-  }
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog
+    .find({})
+    .populate('user', { username: 1, name: 1, id: 1 });
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const blogBase = { ...request.body };
-  try {
-    // const decodedToken = request.token
-    //   ? jwt.verify(request.token, process.env.SECRET)
-    //   : null;
-    // if (!request.token || !decodedToken.id) {
-    //   return response.status(401).json({ error: 'token missing or invalid' })
-    // }
-    // const userToUpdate = await User.findById(decodedToken.id);
+  // const decodedToken = request.token
+  //   ? jwt.verify(request.token, process.env.SECRET)
+  //   : null;
+  // if (!request.token || !decodedToken.id) {
+  //   return response.status(401).json({ error: 'token missing or invalid' })
+  // }
+  // const userToUpdate = await User.findById(decodedToken.id);
 
-    // blogBase.user = userToUpdate._id;
-    blogBase.user = '5e53b86d7a766cdcc5d72cce';
-    const blog = new Blog(blogBase)
-    const savedBlog = await blog.save();
+  // blogBase.user = userToUpdate._id;
+  blogBase.user = '5e53b86d7a766cdcc5d72cce';
+  const blog = new Blog(blogBase)
+  const savedBlog = await blog.save();
 
-    // userToUpdate.blogs.push(savedBlog._id);
-    // await userToUpdate.save();
+  // userToUpdate.blogs.push(savedBlog._id);
+  // await userToUpdate.save();
 
-    response.status(201).json(savedBlog);
-  } catch(e) {
-    next(e);
-  }
+  response.status(201).json(savedBlog);
 })
 
-blogsRouter.delete('/:id', async (req, res, next) => {
-  try {
-    const decodedToken = req.token
-      ? jwt.verify(req.token, process.env.SECRET)
-      : null;
-    if (!req.token || !decodedToken.id) {
-      return res.status(401).json({ error: 'token missing or invalid' })
-    }
-    const blogToDelete = await Blog.findById(req.params.id, 'user');
-    if (decodedToken.id !== blogToDelete.user.toString()) {
-      return res.status(401).json({ error: 'token invalid' })
-    }
-    await Blog.findByIdAndRemove(req.params.id);
-    res.status(204).end();
-  } catch(exception) {
-    next(exception);
+blogsRouter.delete('/:id', async (req, res) => {
+  const decodedToken = req.token
+    ? jwt.verify(req.token, process.env.SECRET)
+    : null;
+  if (!req.token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
   }
+  const blogToDelete = await Blog.findById(req.params.id, 'user');
+  if (decodedToken.id !== blogToDelete.user.toString()) {
+    return res.status(401).json({ error: 'token invalid' })
+  }
+  await Blog.findByIdAndRemove(req.params.id);
+  res.status(204).end();
 })
 
-blogsRouter.put('/:id', async (req, res, next) => {
-  try {
-    const resp = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    res.json(resp.toJSON())
-  } catch(e) {
-    next(e);
-  }
+blogsRouter.put('/:id', async (req, res) => {
+  const resp = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(resp.toJSON())
 })
 
 module.exports = blogsRouter;
--- osa4/src/app.js
+++ osa4/src/app.js
@@ -1,5 +1,6 @@
 // const http = require('http')
 const express = require('express')
+require('express-async-errors');
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
