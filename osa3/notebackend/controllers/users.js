70b5e1cc55aea9f37efe0f8a05d96736659518c6
Paul Vinson Brown <paul.brown@mukavait.fi>
2022-11-13 21:00:50 +0200

    Add `express-async-errors`

--- osa3/notebackend/controllers/users.js
+++ osa3/notebackend/controllers/users.js
@@ -10,55 +10,40 @@ usersRouter.get('/', async (req, res) => {
     res.json(users.map(user => user.toJSON()));
 })
 
-usersRouter.get('/:id', async (req, res, next) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (user) {
-            res.json(user.toJSON())
-        } else {
-            res.status(404).end()
-        }
-    } catch(exception) {
-        next(exception);
+usersRouter.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+        res.json(user.toJSON())
+    } else {
+        res.status(404).end()
     }
 })
 
-usersRouter.post('/', async (req, res, next) => {
-    try {
-        const body = req.body;
+usersRouter.post('/', async (req, res) => {
+    const body = req.body;
 
-        const saltRounds = 10;
-        const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
-        const user = new User ({
-            username: body.username,
-            name: body.name,
-            passwordHash,
-            notes: [],
-        });
+    const user = new User ({
+        username: body.username,
+        name: body.name,
+        passwordHash,
+        notes: [],
+    });
 
-        const savedUser = await user.save();
-        res.json(savedUser.toJSON());
-    } catch(exception) {
-        next(exception);
-    }
+    const savedUser = await user.save();
+    res.json(savedUser.toJSON());
 })
 
-usersRouter.delete('/:id', async (req, res, next) => {
-    try {
-        await User.findByIdAndRemove(req.params.id);
-        res.status(204).end();
-    } catch(exception) {
-        next(exception);
-    }
+usersRouter.delete('/:id', async (req, res) => {
+    await User.findByIdAndRemove(req.params.id);
+    res.status(204).end();
 })
 
-usersRouter.put('/:id', (req, res, next) => {
-    User.findByIdAndUpdate(req.params.id, req.body, { new: true })
-        .then((resp) => {
-            res.json(resp.toJSON())
-        })
-        .catch(error => next(error))
+usersRouter.put('/:id', async (req, res) => {
+    const editedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.json(editedUser);
 })
 
 module.exports = usersRouter;
--- osa4/controllers/users.js
+++ osa4/controllers/users.js
@@ -4,54 +4,38 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 const userConstants = require('../models/userConstants');
 
-usersRouter.get('/', async (request, response, next) => {
-    try {
-        const users = await User
-            .find({})
-            .populate('blogs', { title: 1, url: 1, author: 1 });
-        response.json(users.map(user => user.toJSON()))
-    } catch(e) {
-        next(e);
-    }
+usersRouter.get('/', async (request, response) => {
+    const users = await User
+        .find({})
+        .populate('blogs', { title: 1, url: 1, author: 1 });
+    response.json(users.map(user => user.toJSON()))
 })
 
-usersRouter.post('/', async ({ body }, response, next) => {
-    try {
-        if(body.password.length < userConstants.MIN_PASSWORD_LENGTH) {
-            return response.status(401).json({ error: `Password must be at least ${userConstants.MIN_PASSWORD_LENGTH} characters long` });
-        }
-
-        const newUser = {
-            ...body
-        };
-        const saltRounds = 10;
-        newUser.passwordHash = await bcrypt.hash(body.password, saltRounds);
-        delete newUser.password;
-
-        const user = new User(newUser)
-        const result = await user.save();
-        response.status(201).json(result.toJSON());
-    } catch(e) {
-        next(e);
+usersRouter.post('/', async ({ body }, response) => {
+    if(body.password.length < userConstants.MIN_PASSWORD_LENGTH) {
+        return response.status(401).json({ error: `Password must be at least ${userConstants.MIN_PASSWORD_LENGTH} characters long` });
     }
+
+    const newUser = {
+        ...body
+    };
+    const saltRounds = 10;
+    newUser.passwordHash = await bcrypt.hash(body.password, saltRounds);
+    delete newUser.password;
+
+    const user = new User(newUser)
+    const result = await user.save();
+    response.status(201).json(result.toJSON());
 })
 
-// usersRouter.delete('/:id', async (req, res, next) => {
-//   try {
+// usersRouter.delete('/:id', async (req, res) => {
 //     await User.findByIdAndRemove(req.params.id);
 //     res.status(204).end();
-//   } catch(exception) {
-//     next(exception);
-//   }
 // })
 //
-// usersRouter.put('/:id', async (req, res, next) => {
-//   try {
+// usersRouter.put('/:id', async (req, res) => {
 //     const resp = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
 //     res.json(resp.toJSON())
-//   } catch(e) {
-//     next(e);
-//   }
 // })
 
 module.exports = usersRouter;
