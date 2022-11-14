90214bb3393d0f08d5d68de2f9ae7fe5eb1614e8
Paul Vinson Brown <paul.brown@mukavait.fi>
2022-11-14 21:39:53 +0200

    Change index.js per newer React

--- osa5/src/index.js
+++ osa5/src/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './App';
+
+createRoot(document.getElementById('root')).render(<App />)
